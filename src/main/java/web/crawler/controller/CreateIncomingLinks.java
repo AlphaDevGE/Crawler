d6f9ee5d9996cf0bc783a68502a5cb1ccc3510e9
Mohammad Yazdani <m.yazdani2010@gmail.com>
2016-03-11 21:27:15 -0800

    new Algo

--- src/main/java/web/crawler/controller/CreateIncomingLinks.java
+++ src/main/java/web/crawler/controller/CreateIncomingLinks.java
@@ -3,6 +3,7 @@ package web.crawler.controller;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import web.crawler.db.dao.DocDao;
 import web.crawler.db.model.Address;
@@ -21,6 +22,7 @@ public class CreateIncomingLinks {
 		
 		for(Doc d: docs)
 		{
+			System.out.println("****** Looking at Doc: " + d.getUrl());
 			System.out.println("****** No of OutgoingUrl found: " + d.getOutgoingDocsStr().size() );
 			int outgoingSize = d.getOutgoingDocsStr().size();
 			//create the outgoing Address for current opening Doc
@@ -34,22 +36,39 @@ public class CreateIncomingLinks {
 				//if corresponding outgoing Doc found in DB
 				if( outgoingDoc != null)
 				{
-					System.out.println("**** OutgoingUrl Found: " + outgoingDoc.getUrl() );
-					if(outgoingDoc.getIncomingDocsStr() == null)
+					System.out.println("*** OutgoingUrl Found: " + outgoingDoc.getUrl() );
+					
+					Set<String> incomings = new HashSet<String>();
+					if(incomings == null)
+					{
 						outgoingDoc.setIncomingDocsStr(new HashSet<String>());
+						System.out.println("*** New Incoming String HashSet created for outgoing Doc." + outgoingDoc.getUrl());
+					}
+					for(String s : outgoingDoc.getIncomingDocsStr())
+						incomings.add(s);
+					
+					//add the new incoming
+					incomings.add(d.getUrl());
+					
+					//replace the old incomingDocStr with new one
+					outgoingDoc.setIncomingDocsStr(incomings);
+						
 					if(outgoingDoc.getIncomingAddresses() == null)
+					{
 						outgoingDoc.setIncomingAddresses(new HashSet<Address>());
+						System.out.println("*** New Incoming Address HashSet created for outgoing Doc url: " + outgoingDoc.getUrl());
+					}
 					
 					Address outgoingDocAddress = new Address(
 							outgoingDoc.getUrl(), outgoingDoc.getPath(), outgoingDoc.getOutgoingAddresses().size()); 				
 					d.getOutgoingAddresses().add(outgoingDocAddress);
 					
 					Address currentDocAddress = new Address(d.getUrl(), d.getPath(), outgoingSize);
-					outgoingDoc.getIncomingAddresses().add(currentDocAddress);
+					outgoingDoc.getIncomingAddresses().add(currentDocAddress);					
 					
-					outgoingDoc.getIncomingDocsStr().add(d.getUrl());
+//					outgoingDoc.getIncomingDocsStr().add(d.getUrl());
 					//save to DB
-//					docDao.updateIncommingLink(outgoingUrl, outgoingDoc.getIncomingDocsStr());
+					docDao.updateIncommingLink(outgoingUrl, outgoingDoc.getIncomingDocsStr());
 					docDao.saveDoc(outgoingDoc);
 					
 				}
