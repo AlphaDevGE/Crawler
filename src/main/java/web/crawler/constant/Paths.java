5e5700175dc28d4b443479834db1ab01da9007ad
SavinSachdev <savin.sachdev@yahoo.co.in>
2016-03-17 16:41:08 -0700

    constanat

---
+++ src/main/java/web/crawler/constant/Paths.java
@@ -0,0 +1,6 @@
+package web.crawler.constant;
+
+public class Paths {
+	public static final String PATH_TO_APPLY_INDEXING= "D:/en";
+	public static final String PATH_TO_STORE_INDEXING= "D:/webcrawler/Indexing";
+}
