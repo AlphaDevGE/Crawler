4c791fb7e11da7a0d4c7d99e13aac37e10f9c335
Mohammad Yazdani <m.yazdani2010@gmail.com>
2016-03-20 13:58:46 -0700

    scalechange in pageranking

--- src/main/java/web/crawler/constant/Value.java
+++ src/main/java/web/crawler/constant/Value.java
@@ -11,7 +11,7 @@ public class Value {
 	public static final double TITLE_RANKING_WEIGHT = 1.1; //10% increment
 	public static final double LAMBDA = 0.85;
 	//SCALER is a value that used in page ranking to keep the page ranking for all the pages ranging from 0 to 1
-	public static final double SCALER = 0.75;
+	public static final double SCALER = 0.139;
 	public static final double SCORE_SCALER = 0.95;
 	
 }
